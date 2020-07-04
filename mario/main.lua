e4ef466a3937996a34ee9601e2f2e8c3733b948a
GuillermoCalderaro <guillermocalderaro@gmail.com>
2020-07-04 17:36:11 -0300

    Adaptamos para que mario sea una maquina de estados

--- mario/main.lua
+++ mario/main.lua
@@ -12,14 +12,19 @@ function love.load()
       x = 0,
       y = 0,
       direccion = 1,
-      sprites = {},
+      estados = {
+        standing = {},
+        jumping = {},
+        walking = {},
+        ducking = {}
+      },
+      i = 1,
+      estado = 'standing', 
       img = nil,
-      i= 4
+      speed = 100
     }
-  
     loadMario()  
-    --imgL = love.graphics.newImage("assets/luigi.png")
-    --luigiIMG = love.graphics.newQuad(208, 50,28,38, imgL:getDimensions())
+
     push:setupScreen(virtualWidth, virtualHeight, windowWidth, windowHeight, {
       fullscreen = false,
       resizable = true
@@ -27,14 +32,17 @@ function love.load()
   end
 
 function love.draw()      
-      push:apply('start')
+      push:apply('start')      
       love.graphics.draw(player1.img,
-        player1.sprites[player1.i],
+        player1.estados[player1.estado][player1.i],
         player1.x,
         player1.y,
         0,
         player1.direccion,
-        1)    
+        1, 
+        0, 
+        0)    
+
       push:apply('end')
 end
 
@@ -45,48 +53,64 @@ function loadMario()
   player1.img:setFilter('nearest', 'nearest')
   
   for i=1,3 do 
-    player1.sprites[i]=love.graphics.newQuad(step, 52, 16, 32, player1.img:getDimensions())
+    player1.estados.walking[i]=love.graphics.newQuad(step, 52, 16, 32, player1.img:getDimensions())
     step = step + 30
   end
-  player1.sprites[4]=love.graphics.newQuad(209, 52, 16, 32, player1.img:getDimensions())  
+  player1.estados.standing[1] = love.graphics.newQuad(209, 52, 16, 32, player1.img:getDimensions())  
+
+  player1.estados.ducking[1] = love.graphics.newQuad(389, 52, 16, 32, player1.img:getDimensions()) 
 end
 
 function movimiento()
-  player1.i= math.fmod(player1.i,4) + 1
+  player1.i = math.fmod(player1.i, #player1.estados[player1.estado]) + 1  
 end
 
 
 function love.update(dt)
  (require "lurker").update(dt)
  
- if(love.keyboard.isDown('right'))then
-   player1.x=player1.x+100 * dt
-   player1.direccion = 1
-   movimiento()
+ if  player1.estado ~= 'ducking' then
+   if(love.keyboard.isDown('right'))then
+     player1.x=player1.x+player1.speed * dt
+     player1.direccion = 1
+     player1.estado = 'walking'
+     movimiento()
+   end
+   if love.keyboard.isDown('left') then
+     player1.x=player1.x-player1.speed * dt
+     player1.direccion = -1
+     player1.estado = 'walking'
+     movimiento()
+   end
  end
- if love.keyboard.isDown('left') then
-   player1.x=player1.x-100 * dt
-   player1.direccion = -1
-   movimiento()
- end
+
  if love.keyboard.isDown('up')then
         player1.y= player1.y-10
  end
  gravedad()
+ if love.keyboard.isDown('down') then
+  player1.estado = 'ducking'
+  player1.y = player1.y + 4 
+ end
+  
 end
+
 function love.keyreleased(key)
   if key=='right' or key == 'left'then
-    player1.i=4
+    player1.estado = "standing"
+    player1.i = 1
+  end
+  if key =='down' then
+    player1.y = player1.y - 4
+    player1.estado = 'standing'
   end
 end
+
 function love.keypressed(key)
  if(key == 'r')then
      love.load()
  end
-
 end
-
-
 
 function gravedad()
     player1.y= math.min(player1.y+2,virtualHeight/2)
