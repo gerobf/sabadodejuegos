802af576fb1278c0a1435247fe0919b735e5b1a1
Benjamin Buccianti <benjamin@buccianti.dev>
2020-07-12 17:53:48 -0300

    agregamos piso

--- mario/main.lua
+++ mario/main.lua
@@ -1,139 +1,151 @@
 push = require 'push'
 
 virtualWidth = 432
-virtualHeight = 243
+virtualHeight = 256
+virtualFloor = (virtualHeight / 16) * 12
 
 windowWidth, windowHeight = love.graphics.getDimensions()
- 
-
 
 function love.load()
-    player1 = {
+   player1 = {
       x = 0,
       y = 0,
       direccion = 1,
       estados = {
-        standing = {},
-        jumping = {},
-        walking = {},
-        ducking = {}
+	 standing = {},
+	 jumping = {},
+	 walking = {},
+	 ducking = {}
       },
       i = 1,
-      estado = 'standing', 
+      estado = 'standing',
       img = nil,
       speed = 100,
       jumpingTimer = 0
-    }
-    loadMario()  
+   }
+   tiles = {
+      img = nil,
+      floor = nil,
+   }
 
-    push:setupScreen(virtualWidth, virtualHeight, windowWidth, windowHeight, {
-      fullscreen = false,
-      resizable = true
-    })
-  end
+   loadMario()
+   loadTiles()
 
-function love.draw()      
-      push:apply('start')  
-      if player1.estados[player1.estado][player1.i] ~= nil then     
-      love.graphics.draw(player1.img,
-        player1.estados[player1.estado][player1.i],
-        player1.x,
-        player1.y,
-        0,
-        player1.direccion,
-        1, 
-        0, 
-        0)    
+   push:setupScreen(virtualWidth, virtualHeight, windowWidth, windowHeight,
+		    { fullscreen = false, resizable = true })
+end
+
+function love.draw()
+   push:apply('start')
+   for x=0,virtualWidth,16 do
+      for y=0,virtualHeight,16 do
+	 if y >= virtualFloor then
+	    love.graphics.draw(tiles.img, tiles.floor, x, y, 0)
+	 end
       end
-      push:apply('end')
+   end
+
+   if player1.estados[player1.estado][player1.i] ~= nil then
+      love.graphics.draw(player1.img,
+			 player1.estados[player1.estado][player1.i],
+			 player1.x,
+			 player1.y,
+			 0,
+			 player1.direccion,
+			 1,
+			 0,
+                         32)
+   end
+
+   push:apply('end')
 end
 
 
 function loadMario()
-  local step=239
-  player1.img = love.graphics.newImage("assets/mario.png")
-  player1.img:setFilter('nearest', 'nearest')
-  
-  for i=1,3 do 
-    player1.estados.walking[i]=love.graphics.newQuad(step, 52, 16, 32, player1.img:getDimensions())
-    step = step + 30
-  end
-  player1.estados.standing[1] = love.graphics.newQuad(209, 52, 16, 32, player1.img:getDimensions())  
+   local step=239
+   player1.img = love.graphics.newImage("assets/mario.png")
+   player1.img:setFilter('nearest', 'nearest')
 
-  player1.estados.ducking[1] = love.graphics.newQuad(389, 52, 16, 32, player1.img:getDimensions()) 
+   for i=1,3 do
+      player1.estados.walking[i]=love.graphics.newQuad(step, 52, 16, 32, player1.img:getDimensions())
+      step = step + 30
+   end
 
-  player1.estados.jumping[1] = love.graphics.newQuad(359, 52, 16,32, player1.img:getDimensions() )
+   player1.estados.standing[1] = love.graphics.newQuad(209, 52, 16, 32, player1.img:getDimensions())
+   player1.estados.ducking[1] = love.graphics.newQuad(389, 52, 16, 32, player1.img:getDimensions())
+   player1.estados.jumping[1] = love.graphics.newQuad(359, 52, 16,32, player1.img:getDimensions() )
+end
 
+function loadTiles()
+   tiles.img = love.graphics.newImage("assets/tiles.png")
+   tiles.floor = love.graphics.newQuad(0, 0, 16, 16, tiles.img:getDimensions())
 end
 
 function movimiento()
-  player1.i = math.fmod(player1.i, #player1.estados[player1.estado]) + 1  
+   player1.i = math.fmod(player1.i, #player1.estados[player1.estado]) + 1
 end
 
-function moverX(signo, dt) 
-  player1.x = player1.x + player1.speed * dt * signo
-     player1.direccion = 1 * signo
-     if player1.estado ~= 'jumping' then
+function moverX(signo, dt)
+   player1.x = player1.x + player1.speed * dt * signo
+   player1.direccion = 1 * signo
+   if player1.estado ~= 'jumping' then
       player1.estado = 'walking'
       movimiento()
-     end
+   end
 end
 
-
-
 function love.update(dt)
- (require "lurker").update(dt)
- 
- if  player1.estado ~= 'ducking' then  
-   if(love.keyboard.isDown('right'))then
-     moverX(1, dt)
+   (require "lurker").update(dt)
+
+   if  player1.estado ~= 'ducking' then
+      if(love.keyboard.isDown('right'))then
+	 moverX(1, dt)
+      end
+      if love.keyboard.isDown('left') then
+	 moverX(-1, dt)
+      end
    end
-   if love.keyboard.isDown('left') then
-     moverX(-1, dt)
+   if player1.estado ~= 'jumping' and love.keyboard.isDown('up')then
+      player1.i = 1
+      player1.estado = 'jumping'
+      player1.jumpingTimer = 16
    end
- end
- if player1.estado ~= 'jumping' and love.keyboard.isDown('up')then
-          player1.i = 1
-          player1.estado = 'jumping'
-          player1.jumpingTimer = 16
-  end
 
-  if player1.estado == 'jumping' and player1.jumpingTimer > 0 then
-    print('Gero que estas comiendo?')
-    player1.jumpingTimer= player1.jumpingTimer - 0.8
-    player1.y = player1.y - 300 * dt
-  end
+   if player1.estado == 'jumping' and player1.jumpingTimer > 0 then
+      player1.jumpingTimer= player1.jumpingTimer - 0.8
+      player1.y = player1.y - 300 * dt
+   end
 
- gravedad()
+   gravedad()
 
- if love.keyboard.isDown('down') then
-  player1.estado = 'ducking'
-  player1.y = player1.y + 4 
- end
-  
+   if love.keyboard.isDown('down') then
+      player1.estado = 'ducking'
+      player1.y = player1.y + 4
+   end
 end
 
 function love.keyreleased(key)
-  if key=='right' or key == 'left'then
-    player1.estado = "standing"
-    player1.i = 1
-  end
-  if key =='down' then
-    player1.y = player1.y - 4
-    player1.estado = 'standing'
-  end
+   if player1.estado ~= 'jumping' and (key == 'right' or key == 'left') then
+      player1.estado = 'standing'
+      player1.i = 1
+   end
+   if key == 'down' then
+      player1.y = player1.y - 4
+      player1.estado = 'standing'
+   end
 end
 
 function love.keypressed(key)
- if(key == 'r')then
-     love.load()
- end
+   if(key == 'r')then
+      love.load()
+   end
 end
 
 function gravedad()
-    player1.y= math.min(player1.y+2,virtualHeight/2)
-    if player1.estado == 'jumping' and player1.y == virtualHeight/2 then
+   player1.y= math.min(player1.y + 2, virtualFloor)
+   if player1.estado == 'jumping' and player1.y == virtualFloor and
+      player1.jumpingTimer <= 0 then
       player1.estado = 'standing'
-    end
+   end
 end
 
